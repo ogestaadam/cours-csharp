83994ee61225d2b8990dcde63721071c619645c7
adam_ogesta <adam_ogesta@yahoo.com>
2017-09-26 16:42:25 +0200

    Add project files.

---
+++ Yours Name/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Yours_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            Console.Write("input yours first name:");
+            name = Console.ReadLine();
+            Console.Write("quelle est votre age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
